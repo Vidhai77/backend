c457d9836382c5ca1f2c1f5edeafcc20ca8a40af
Balamurugan-1000 <rbalamurugan1000@gmail.com>
2025-04-16 13:29:21 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+
 import authRoutes from "./routes/authRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import { connectDb } from "./config/connectDb.js";
@@ -13,11 +13,11 @@ const app = express();
 
 // Middleware
 app.use(express.json()); // Body parser
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://frontend-nu-six-41.vercel.app","https://frontend-git-main-vidhais-projects.vercel.app"],
-  })
-); // Enable CORS (optional, for frontend)
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "http://localhost:3001","https://frontend-nu-six-41.vercel.app","https://frontend-git-main-vidhais-projects.vercel.app"],
+//   })
+// ); // Enable CORS (optional, for frontend)
 
 app.use("/api/auth", authRoutes); // Renaming it to /auth since it's for authentication
 app.use("/api/projects", projectRoutes);
